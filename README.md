083c0d9218c581656409d732ebb1d4717abd9ed4
EduardYan <eduarygp@gmail.com>
2023-11-29 11:51:54 -0600

    random color button added

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # React Show Messages App
 
 ## Screenshot
+
 ![screenshot](./doc/screenshot.png)
+
+## Install
+
+```bash
+corepack enable
+corepack prepare pnpm@latest --activate
+pnpm i
+```
+
+## Run
+
+```bash
+pnpm run dev
+```
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import { Message } from "./components/Message.jsx";
 function App() {
   const [message, setMessage] = useState("");
 
+  const generateColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const rbgColor = `background-color: rgb(${r} ${g} ${b});`;
+    document.body.setAttribute("style", rbgColor);
+  };
+
   return (
     <main>
       <h1>Welcome back!</h1>
@@ -11,13 +20,17 @@ function App() {
       <Message>{message}</Message>
 
       <div>
-        <input type="text" id="message" />
+        <input type="text" id="message" autoFocus />
         <button
           onClick={() => setMessage(document.getElementById("message").value)}
         >
           Show
         </button>
       </div>
+
+      <button id="changeColor" onClick={generateColor}>
+        Change Color
+      </button>
     </main>
   );
 }
